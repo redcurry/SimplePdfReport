3c7b7f7801b7bee208bf751e23eebb6cf4802f6b
Carlos Anderson <carlosjanderson@gmail.com>
2017-09-02 16:08:09 -0400

    Add project to test exporting of report

---
+++ src/SimplePdfReport.Test/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using SimplePdfReport.Reporting;
+
+namespace SimplePdfReport.Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var reportData = CreateReportData();
+        }
+
+        private static ReportData CreateReportData()
+        {
+            return new ReportData
+            {
+                Patient = new Patient
+                {
+                    Id = "38561948",
+                    FirstName = "Daniel",
+                    LastName = "Price",
+                    Sex = Sex.Male,
+                    Birthdate = new DateTime(1950, 1, 1),
+                    Doctor = new Doctor
+                    {
+                        FirstName = "Heather",
+                        LastName = "Powell"
+                    }
+                },
+                StructureSet = new StructureSet
+                {
+                    Id = "CT",
+                    Image = new Image
+                    {
+                        Id = "CT_SCAN",
+                        CreationTime = new DateTime(2017, 9, 2, 15, 56, 12)
+                    },
+                    Structures = new[]
+                    {
+                        new Structure
+                        {
+                            Id = "PTV",
+                            VolumeInCc = 153.83,
+                            MeanDoseInGy = 47.12
+                        },
+                        new Structure
+                        {
+                            Id = "Bladder",
+                            VolumeInCc = 96.31,
+                            MeanDoseInGy = 38.60
+                        },
+                        new Structure
+                        {
+                            Id = "Bowel",
+                            VolumeInCc = 1683.98,
+                            MeanDoseInGy = 34.71,
+                        },
+                        new Structure
+                        {
+                            Id = "Femur, Left",
+                            VolumeInCc = 176.33,
+                            MeanDoseInGy = 15.45
+                        },
+                        new Structure
+                        {
+                            Id = "Femur, Right",
+                            VolumeInCc = 174.43,
+                            MeanDoseInGy = 16.11
+                        },
+                        new Structure
+                        {
+                            Id = "Prostate Bed",
+                            VolumeInCc = 76.67,
+                            MeanDoseInGy = 46.78,
+                        },
+                        new Structure
+                        {
+                            Id = "Rectum",
+                            VolumeInCc = 75.41,
+                            MeanDoseInGy = 26.39
+                        },
+                    }
+                }
+            };
+        }
+    }
+}
